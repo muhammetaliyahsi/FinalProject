414977e89fe0b74f7612f2c502bd29e390a4fc2e
Muhammet Ali Yahşi <alleyy@outlook.com.tr>
2021-03-17 20:13:03 +0300

    10.03.2021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -41,6 +41,7 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
 
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -71,6 +72,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
